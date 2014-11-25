6a9bca1c97c5513bb7ca3a873cb0c258697addee
teppeis <teppeis@gmail.com>
2014-11-26 02:07:21 +0900

    Fix indent

--- test/test.js
+++ test/test.js
@@ -8,79 +8,79 @@ var dereserve = require('../');
 
 describe('dereserve', function() {
   it('should convert reserved keyword.', function(cb) {
-  var stream = dereserve();
+    var stream = dereserve();
 
-  stream.on('data', function(file) {
+    stream.on('data', function(file) {
       assert.equal(file.relative, 'file.js');
       assert.equal(file.contents.toString(), 'a["catch"]()');
-  });
+    });
 
-  stream.on('end', cb);
+    stream.on('end', cb);
 
-  stream.write(new gutil.File({
+    stream.write(new gutil.File({
       base: __dirname,
       path: __dirname + '/file.js',
       contents: new Buffer('a.catch()')
-  }));
+    }));
 
-  stream.end();
+    stream.end();
   });
 
   it('should work with stream.', function(cb) {
-  var stream = dereserve();
+    var stream = dereserve();
 
-  stream.on('data', function(file) {
+    stream.on('data', function(file) {
       assert.equal(file.relative, 'file.js');
       file.contents.pipe(es.wait(function(err, data) {
-          assert(!err);
-          assert.equal(data, 'a["catch"]()');
-          cb();
+        assert(!err);
+        assert.equal(data, 'a["catch"]()');
+        cb();
       }));
-  });
+    });
 
-  stream.write(new gutil.File({
+    stream.write(new gutil.File({
       base: __dirname,
       path: __dirname + '/file.js',
       contents: stringToStream('a.catch()')
-  }));
+    }));
 
-  stream.end();
+    stream.end();
   });
 
   it('should generate source map.', function(cb) {
-  var stream = dereserve();
-  stream.on('data', function(file) {
+    var stream = dereserve();
+    stream.on('data', function(file) {
       assert.equal(file.relative, 'file.js');
       assert.equal(file.contents.toString(), 'a["catch"]()');
       assert.deepEqual(file.sourceMap, {
-      version: 3,
-      sources: ['file.js'],
-      names: [],
-      mappings: 'AAAA,UAAO,CAAC',
-      file: 'file.js',
-      sourcesContent: ['a.catch()']
+        version: 3,
+        sources: ['file.js'],
+        names: [],
+        mappings: 'AAAA,UAAO,CAAC',
+        file: 'file.js',
+        sourcesContent: ['a.catch()']
       });
-  });
+    });
 
-  stream.on('end', cb);
+    stream.on('end', cb);
 
-  var file = new gutil.File({
+    var file = new gutil.File({
       base: __dirname,
       path: __dirname + '/file.js',
       contents: new Buffer('a.catch()')
-  });
-  var inMap = {
+    });
+    var inMap = {
       version : 3,
       file: file.relative,
       names: [],
       mappings: '',
       sources: [file.relative],
       sourcesContent: [file.contents.toString()]
-  };
-  file.sourceMap = inMap;
+    };
+    file.sourceMap = inMap;
 
-  stream.write(file);
+    stream.write(file);
 
-  stream.end();
+    stream.end();
   });
 });
